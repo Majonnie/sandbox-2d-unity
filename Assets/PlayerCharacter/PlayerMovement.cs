5abb86ee92ded910b9156cde1ef67129fd93ebac
Majonnie <bonhommejulie2003@gmail.com>
2024-08-26 17:52:28 +0200

    [FEAT] improved player movement, added jump and friction, added first dummy sprites

--- Assets/PlayerCharacter/PlayerMovement.cs
+++ Assets/PlayerCharacter/PlayerMovement.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
+    public float groundSpeed;
+    public float jumpSpeed;
+    public float acceleration;
+    [Range(0f, 1f)]
+    public float groundDecay;
     public Rigidbody2D body;
+    public BoxCollider2D groundCheck;
+    public LayerMask groundMask;
+    public bool grounded;
+
+    float xInput;
 
     // Start is called before the first frame update
     void Start()
@@ -16,17 +25,54 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xInput = Input.GetAxis("Horizontal");
-        float yInput = Input.GetAxis("Vertical");
+        GetInput();
+        HandleJump();
+    }
 
+    void FixedUpdate()
+    {
+        CheckGround();
+        ApplyFriction();
+        MoveWithInput();
+    }
+
+    void GetInput()
+    {
+        xInput = Input.GetAxis("Horizontal");
+    }
+
+    void MoveWithInput()
+    {
         if (Mathf.Abs(xInput) > 0)
         {
-            body.velocity = new Vector2(xInput * speed, body.velocity.y);
-        }
+            float increment = xInput * acceleration;
+            float newSpeed = Mathf.Clamp(body.velocity.x + increment, -groundSpeed, groundSpeed);
+            body.velocity = new Vector2(newSpeed, body.velocity.y);
 
-        if (Mathf.Abs(yInput) > 0)
+            // Change player direction
+            float direction = Mathf.Sign(xInput);
+            transform.localScale = new Vector3(direction, 1, 1);
+        }
+    }
+
+    void HandleJump()
+    {
+        if (Input.GetButtonDown("Jump") && grounded)
         {
-            body.velocity = new Vector2(body.velocity.x, yInput * speed);
+            body.velocity = new Vector2(body.velocity.x, jumpSpeed);
+        }
+    }
+
+    void CheckGround()
+    {
+        grounded = Physics2D.OverlapAreaAll(groundCheck.bounds.min, groundCheck.bounds.max, groundMask).Length > 0;
+    }
+
+    void ApplyFriction()
+    {
+        if (grounded && xInput == 0 && body.velocity.y <= 0)
+        {
+            body.velocity *= groundDecay;
         }
     }
 }
